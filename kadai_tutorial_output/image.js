e57b49f16f020c1ee76ca07ee1c0824c1e3af8a2
山﨑　博史 <>
2025-03-12 22:07:56 +0900

    ABOUT USの変更

--- kadai_tutorial_output/image.js
+++ kadai_tutorial_output/image.js
@@ -8,6 +8,10 @@ const body = document.body;//bodyを取得
 const accordionHeaders = document.querySelectorAll(".js-accordion-item");
 const accordionContents = document.querySelectorAll(".js-accordion-contents");
 
+const cultureNums = document.querySelectorAll('.culture-num');
+const cultureEns = document.querySelectorAll('.culture-en');
+const cultureImgs = document.querySelectorAll('.culture-img1, .culture-img2, .culture-img3');
+
 //forEachは配列の要素を一つずつ取り出して処理を行うメソッドです
 modalOpen.forEach(item => {//item（名前はなんでも良い）は引数と呼ばれるもので、配列の要素を一つずつ取り出して処理を行う際に使います
     
@@ -42,5 +46,35 @@ accordionHeaders.forEach(header => {
 
         // 開閉の切り替え
         content.classList.toggle("is-show");
+    });
+});
+
+// クリックイベントを追加　ABOUTO US画像の切り替え
+cultureNums.forEach((num, index) => {
+    num.addEventListener("click", () => {
+        // すべての画像を非表示にする
+        cultureImgs.forEach(img => img.classList.remove('is-show'));
+
+        // クリックされた番号に対応する画像を表示
+        if (index === 0) {
+            document.querySelector('.culture-img1').classList.add('is-show');
+        } else if (index === 1) {
+            document.querySelector('.culture-img2').classList.add('is-show');
+        } else if (index === 2) {
+            document.querySelector('.culture-img3').classList.add('is-show');
+        }
+    });
+});
+
+// クリックイベントを追加 ABOUTO USの文字の色の変化
+cultureNums.forEach((num, index) => {
+    num.addEventListener("click", () => {
+        // すべての要素の色をリセット
+        cultureNums.forEach(num => num.classList.remove('is-show'));
+        cultureEns.forEach(en => en.classList.remove('is-show'));
+
+        // クリックした番号に対応する要素の色を変更
+        cultureNums[index].classList.add('is-show');
+        cultureEns[index].classList.add('is-show');
     });
 });
